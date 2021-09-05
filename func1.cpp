7e91cd1b05473afc3ae3e73c4d4ed555e4da722f
Tatev Avoyan <Tatev.avoyan@yahoo.com>
2021-09-05 23:16:55 +0400

    added func1

--- func1.cpp
+++ func1.cpp
@@ -1,3 +1,11 @@
 #include "iostream"
 #include "func1.h"
 
+int num(int start, int end){
+
+        int sum = 0;
+        for (; start <= end; start++)
+            sum = sum + start;
+
+    return sum;
+}
