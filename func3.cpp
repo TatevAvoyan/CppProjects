17fe3489a4a1df06e508aaf13bf3c1b3d5a44dc0
Tatev Avoyan <Tatev.avoyan@yahoo.com>
2021-09-06 23:55:32 +0400

    Added func3

--- func3.cpp
+++ func3.cpp
@@ -1,2 +1,27 @@
 #include "iostream"
 #include "func3.h"
+using namespace std;
+
+void mass(int i, int mas[5]){
+
+    int min;
+    int max;
+
+    for (; i<5; i++){
+        max = mas[0];
+        min = mas[0];
+    }
+    for (i=1; i<5; i++){
+
+        if (min <= mas[i]){
+            max = mas[i];
+        }
+    }
+    for (i=1; i<5; i++){
+
+        if (min >= mas[i]){
+            min = mas[i];
+        }
+    }
+    cout << "Min number is " << min << ", Max number is " << max << endl;
+}
--- func3.h
+++ func3.h
@@ -1,1 +1,3 @@
 #pragma once
+
+void mass(int i, int mas[5]);
