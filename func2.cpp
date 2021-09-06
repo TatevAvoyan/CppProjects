eadd875316bcd71f6507754445c83fbaf2ef357c
Tatev Avoyan <Tatev.avoyan@yahoo.com>
2021-09-06 21:07:00 +0400

    Added func2

--- func2.cpp
+++ func2.cpp
@@ -1,2 +1,17 @@
 #include "iostream"
 #include "func2.h"
+using namespace std;
+
+void arr(int num, int arr1[]){
+
+    int len = 8;
+    for (int i = 0; i < len ; i++){
+
+        if (num > arr1[i]){
+            continue;
+        } else{
+            num = arr1[i];
+            cout << num << ", ";
+        }
+    }
+}
--- func2.h
+++ func2.h
@@ -1,1 +1,3 @@
 #pragma once
+
+void arr(int num, int arr1[]);
