9e21462cae9da384e5cc50d895b105170df785e6
Tatev Avoyan <Tatev.avoyan@yahoo.com>
2021-09-07 14:13:09 +0400

    Added func 7

--- func7.cpp
+++ func7.cpp
@@ -1,2 +1,13 @@
 #include "iostream"
 #include "func7.h"
+using namespace std;
+
+void factorial(int number) {
+
+    int unsigned fact = 1;
+    int unsigned i = 1;
+    for (; i <= number; i++) {
+        fact = fact * i;
+    }
+    cout << "Factorial of " << number << " is: " << fact << "\n" << endl;
+}
--- func7.h
+++ func7.h
@@ -1,1 +1,3 @@
 #pragma once
+
+void factorial(int number);
