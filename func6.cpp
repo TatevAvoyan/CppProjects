b450906ded47c35d9fb34e048e3143a3c507d6b3
Tatev Avoyan <Tatev.avoyan@yahoo.com>
2021-09-07 13:53:18 +0400

    Added func6

--- func6.cpp
+++ func6.cpp
@@ -1,2 +1,13 @@
 #include "iostream"
 #include "func6.h"
+using namespace std;
+
+void loop(int start1, int end1, int min){
+
+    for(; start1 < end1; start1++) {
+        if (start1 < min) {
+            continue;
+        }
+        cout << start1 << "\n";
+    }
+}
--- func6.h
+++ func6.h
@@ -1,1 +1,3 @@
 #pragma once
+
+void loop(int start1, int end1, int min);
